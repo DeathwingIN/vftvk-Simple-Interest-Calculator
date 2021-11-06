b3a0b9936142349687e604d65f1c00acfe5424fb
DeathwingIN <jainjayakody@std.appsc.sab.ac.lk>
2021-11-06 22:33:22 +0530

    final

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function compute()
+function simple_interest()
 {
-    p = document.getElementById("principal").value;
-    
+
+var a,t,r,si,ny;
+a = document.getElementById ("amount").value;
+t = document.getElementById ("time").value;
+r = document.getElementById ("rate").value;
+si = ((a*r*t)/100);
+const d = new Date();
+let year = d.getFullYear();
+ny = (t+year);
+document.getElementById ('lamount').innerHTML ="If You Deposit";
+document.getElementById ('amountl1').innerHTML =+a;
+document.getElementById ('lrate').innerHTML ="At an Interest Rate of";
+document.getElementById ('ratel').innerHTML =+r;
+document.getElementById ('linterst').innerHTML ="You will Recive an Amount of";
+document.getElementById ('interstl').innerHTML =+si;
+document.getElementById ('lyear').innerHTML ="In the Year";
+document.getElementById ('yearl').innerHTML =+ny;
 }
-        
