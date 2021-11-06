8fcb3cde383396861042069f94f8f3a4dbe18c22
DeathwingIN <jainjayakody@std.appsc.sab.ac.lk>
2021-11-06 22:33:39 +0530

    final

--- script.js
+++ script.js
@@ -5,16 +5,26 @@ var a,t,r,si,ny;
 a = document.getElementById ("amount").value;
 t = document.getElementById ("time").value;
 r = document.getElementById ("rate").value;
+if ((!a.match(/^\d+/)))
+     {
+      alert('Please Enter Valid Amount !!!!');
+      document.getElementById('amount').focus();
+      return;
+    }
 si = ((a*r*t)/100);
-const d = new Date();
-let year = d.getFullYear();
-ny = (t+year);
+ny = new Date().getFullYear() + parseInt(t);
 document.getElementById ('lamount').innerHTML ="If You Deposit";
 document.getElementById ('amountl1').innerHTML =+a;
 document.getElementById ('lrate').innerHTML ="At an Interest Rate of";
 document.getElementById ('ratel').innerHTML =+r;
-document.getElementById ('linterst').innerHTML ="You will Recive an Amount of";
+document.getElementById ('linterst').innerHTML ="You will Recive an Amount of ";
 document.getElementById ('interstl').innerHTML =+si;
 document.getElementById ('lyear').innerHTML ="In the Year";
 document.getElementById ('yearl').innerHTML =+ny;
 }
+function updateRate() 
+{
+    var rateval = document.getElementById('rate').value;
+    document.getElementById('rate_val').innerText = rateval;
+}
+
